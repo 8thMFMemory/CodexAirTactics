b14d5c65c70c13df83154567e39ccdee73fd22b0
Federico Farina <50364493+8thMFMemory@users.noreply.github.com>
2022-10-08 18:31:06 +0200

    Add files via upload

---
+++ pages/🔧 Lista Mod.py
@@ -0,0 +1,112 @@
+import streamlit as st
+import pandas as pd
+import os
+
+st.set_page_config(
+    page_title="Lista Mod",
+    page_icon="🔧",
+    layout="wide",
+    initial_sidebar_state="auto"
+)
+st.header("Lista Mod")
+st.subheader("Cerca e trova la Mod adatta!")
+modpath=str(r'D:\feder\OneDrive\Documenti\Programming\StreamLitCodex\data\ModList.xlsx')
+
+@st.cache
+def getmodlist():
+    global modlist
+    modlist=pd.read_excel(io=modpath,sheet_name=0,header=0,index_col=0)
+    return modlist
+
+modlist=getmodlist()
+st.write("🔎 Seleziona una cella e premi CTRL+F per cercare")
+
+
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+
+
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Aggiungi filtri")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filtra Mod per", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Valori per {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Valori per {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Valor per {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Testo o regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
+
+st.dataframe(filter_dataframe(modlist),use_container_width=True)
+
+
+
