71512301d0d92fc3ccc916578871b0548635bf07
Federico Farina <50364493+8thMFMemory@users.noreply.github.com>
2022-10-12 01:36:59 +0200

    Update Codex_Encounter_Builder.py

--- Codex_Encounter_Builder.py
+++ Codex_Encounter_Builder.py
@@ -18,7 +18,7 @@ with colx:
     #st.subheader(st.text_input(label="Nome Scontro",label_visibility="hidden"))
 #ho moddato il codice sorgente per non fare l'errore - file utils.py righe commentate
 # Resource Variables
-modpath='.data/ModList.xlsx'
+modpath='./data/ModList.xlsx'
 @st.cache
 def getmodlist():
     global modlist
